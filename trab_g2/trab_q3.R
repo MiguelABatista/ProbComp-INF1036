e5e1f8d56b15d7edd42d1adae52a47dc48eabf6a
MiguelABatista <55413505+MiguelABatista@users.noreply.github.com>
2023-06-14 21:15:34 -0300

    Update trab_q3.R

--- trab_g2/trab_q3.R
+++ trab_g2/trab_q3.R
@@ -1,11 +1,53 @@
-letra_a <- function(){
+testa_c <- function(c,nsamples){
+  sum = 0
+  for(i in 1:nsamples){
+    x = runif(1)
+    sum = sum + 2*(2*c*x+1)
+  }
+  return(sum/nsamples)
+}
+
+letra_a <- function(nsamples){
   f <- function(x,c){
     return(c*x+1)
   }
   for(i in 1:20){
     c = -1.5+i/10
-    print(c)
-    print(integrate(f,0,2,c))
+    resp = testa_c(c,nsamples)
+    if(abs(resp - 1) < 1/100){
+      return(c)
+    }
   }
+  return("Nenhum c satisfaz")
 }
-letra_a()
+
+c = letra_a(10000)
+print("Letra a:")
+print(c)
+
+letra_b <- function(c, nsamples){
+  sum = 0
+  for(i in 1:nsamples){
+    x = runif(1)
+    sum = sum + c*x+1
+  }
+  return(sum/nsamples)
+}
+
+letra_c <- function(c, nsamples){
+    sum = 0
+  for(i in 1:nsamples){
+    x = runif(1)
+    sum = sum + (2*x)*(2*c*x+1)*2
+  }
+  return(sum/nsamples)
+}
+c = letra_a(10000)
+print("Letra a:")
+print(c)
+
+print("Letra b:")
+print(letra_b(c,100000))
+
+print("Letra c:")
+print(letra_c(c,100000))
